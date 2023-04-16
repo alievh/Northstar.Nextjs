bcf6fa7d5c36879a9df228add0a99c2caea3e5d4
Hüseyn Alıyev <huseynma@code.edu.az>
2023-04-17 02:39:15 +0400

    feat: Added Contact Page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
-import BannerImage from "../assets/images/contact_banner.jpg"
+import BannerImage from "../assets/images/contact_banner.jpg";
 import PageHeadBanner from "@/components/PageHeadBanner";
+import { Arimo } from "next/font/google";
+
+const arimo = Arimo({ weight: "700", subsets: ["latin"] });
 
 interface contactProps {}
 
@@ -9,6 +12,57 @@ const contact: FC<contactProps> = ({}) => {
     <main>
       <section className="contact-banner-section">
         <PageHeadBanner src={BannerImage.src} heading="Contact Us" />
+      </section>
+      <section className="contact-form-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8">
+              <h4 className={arimo.className}>
+                We would love to hear from you.
+              </h4>
+              <p>
+                If you have any query or any type of suggestion, you can contact
+                us here. We would love to hear from you.
+              </p>
+              <form method="post">
+                <div className="form-top">
+                  <div className="row w-100">
+                    <div className="form-name col-lg-6">
+                      <label>Name</label>
+                      <input></input>
+                    </div>
+                    <div className="form-email col-lg-6">
+                      <label>Email</label>
+                      <input></input>
+                    </div>
+                  </div>
+                </div>
+                <div className="row w-100">
+                  <div className="form-message col-lg-12">
+                    <label>Message</label>
+                    <textarea rows={12}></textarea>
+                  </div>
+                </div>
+              </form>
+            </div>
+            <div className="col-lg-4">
+              <div className="visit-us">
+                <h5 className={arimo.className}>Visit Us</h5>
+                <ul>
+                  <li>UET Lahore, Punjab, Pakistan</li>
+                  <li>Phone: +923039898987</li>
+                </ul>
+              </div>
+              <div className="get-in-touch">
+                <h5 className={arimo.className}>Get In Touch</h5>
+                <ul>
+                  <li>You can get in touch with us on this provided email.</li>
+                  <li>Email: hmjawad087@gmail.com</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
