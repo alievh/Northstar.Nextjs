f341556d02ed7aea863b40892a9b8161aada0394
Hüseyn Alıyev <huseynma@code.edu.az>
2023-04-18 12:33:59 +0400

    fix: Refactor SingleProduct Page Responsive

--- components/BenefitCard.tsx
+++ components/BenefitCard.tsx
@@ -2,19 +2,17 @@ import { Baloo_2 } from "next/font/google";
 import { FC, ReactNode } from "react";
 
 interface BenefitCardProps {
-  icon: any;
+  icon: string;
   header: string;
   text: string;
 }
 
-const baloo = Baloo_2({weight: "700" ,subsets: ["latin"]})
+const baloo = Baloo_2({ weight: "700", subsets: ["latin"] });
 
 const BenefitCard: FC<BenefitCardProps> = ({ icon, header, text }) => {
   return (
     <div className="col-lg-3 col-sm-6 benefit">
-      <div className="benefit__icons">
-        {icon}
-      </div>
+      <div className="benefit__icons">{icon}</div>
       <div className="benefit__content">
         <h5 className={baloo.className}>{header}</h5>
         <p>{text}</p>
